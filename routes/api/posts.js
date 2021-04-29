711495e905c0c1ee7b60e98d564f3d46e428f44a
pancho1337 <pancho404x@gmail.com>
2021-04-29 00:23:18 -0500

    added like unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,4 +84,35 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+router.put('/like/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({ msg: 'post already liked' })
+        }
+        post.likes.unshift({ user: req.user.id })
+        await post.save()
+        res.json(post.likes)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error on like post')
+    }
+})
+
+router.put('/unlike/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({ msg: 'post not yet liked' })
+        }
+        const removeIndex = post.likes.map( like => like.user.toString() ).indexOf(req.user.id)
+        post.likes.splice(removeIndex, 1)
+        await post.save()
+        res.json(post.likes)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error on unlike post')
+    }
+})
+
 module.exports = router
