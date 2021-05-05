0ffd0538102e7332a07f16abde47782d9fc5fb13
pancho1337 <pancho404x@gmail.com>
2021-05-04 23:26:38 -0500

    react router links added auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/navbar'
 import Landing from './components/layouts/landing'
 
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
 const App = () => 
+<Router>
   <Fragment className="App">
     <Navbar />
-    <Landing />
+    <Route exact path="/" component={ Landing } />
+    <section className='container'>
+      <switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </switch>
+    </section>
   </Fragment>
+</Router>
+
 
 
 export default App;
