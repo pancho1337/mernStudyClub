ca7c68179abd61dad6416391cce21c70a0160bf5
pancho1337 <pancho404x@gmail.com>
2021-05-06 22:24:46 -0500

    stareted up redux with thunk

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Fragment } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/navbar'
 import Landing from './components/layouts/landing'
@@ -7,21 +6,28 @@ import Landing from './components/layouts/landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
+import { Provider } from 'react-redux'
+import store  from './store.js'
+
+import './App.css';
+
 const App = () => 
-<Router>
-  <div className="App">
-  <Fragment>
-    <Navbar />
-    <Route exact path="/" component={ Landing } />
-    <section className='container'>
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </section>
-  </Fragment>
-  </div>
-</Router>
+<Provider store={store}>
+  <Router>
+    <div className="App">
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={ Landing } />
+      <section className='container'>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+    </div>
+  </Router>
+</Provider>
 
 
 
