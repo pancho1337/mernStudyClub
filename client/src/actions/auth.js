e61fb5e9d553337479520bc99d3893ee6c74c1cd
pancho1337 <pancho404x@gmail.com>
2021-05-12 00:02:01 -0500

    user login, Redirect

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS
 } from './types'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -40,6 +42,7 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         })
+        dispatch(loadUser())
     } catch (error) {
         const errors = error.response.data.errors
         if(errors){
@@ -47,6 +50,32 @@ export const register = ({ name, email, password }) => async dispatch => {
         }
         dispatch({
             type: REGISTER_FAIL
+        })
+    }
+}
+
+// login user  
+export const login = ( email, password ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const body = JSON.stringify({ email, password})
+    try {
+        const res = await axios.post('/api/auth', body, config)
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+        dispatch(loadUser())
+    } catch (error) {
+        const errors = error.response.data.errors
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+        dispatch({
+            type: LOGIN_FAIL
         })
     }
 }
